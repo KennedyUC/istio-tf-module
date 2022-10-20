1de6de7fca006d517f5a726c3eef3c0c19322188
kennedyuc <chukwukakennedyuche@gmail.com>
2022-10-20 12:26:36 -0700

    file updates

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ resource "null_resource" "manage-ns" {
 resource "helm_release" "istio_base" {
   depends_on       = [null_resource.manage_ns]
   name             = "istio-base"
-  repository       = "https://istio-release.storage.googleapis.com/charts"
+  repository       = var.istio_chart_repo
   chart            = "base"
-  namespace        = "istio-system"
-  create_namespace = false
+  namespace        = var.istio_namespace
+  create_namespace = var.helm_create_namespace
   values           = []
   atomic           = true
 }
@@ -21,10 +21,10 @@ resource "helm_release" "istio_base" {
 resource "helm_release" "istio_d" {
   depends_on       = [helm_release.istio_base]
   name             = "istio-d"
-  repository       = "https://istio-release.storage.googleapis.com/charts"
+  repository       = var.istio_chart_repo
   chart            = "istiod"
-  namespace        = "istio-system"
-  create_namespace = false
+  namespace        = var.istio_namespace
+  create_namespace = var.helm_create_namespace
   values           = []
   atomic           = false
 }
